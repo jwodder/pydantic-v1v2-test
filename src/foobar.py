534f130c4bda26a0ed2eff5f8877f6ffabd27ed5
John T. Wodder II <git@varonathe.org>
2023-06-01 14:03:48 -0400

    Test running & type-checking under both v1 and v2 of Pydantic

--- src/foobar.py
+++ src/foobar.py
@@ -6,23 +6,23 @@ Visit <https://github.com/jwodder/pydantic-v1v2-test> for more information.
 
 from __future__ import annotations
 from pathlib import Path
-
-__version__ = "0.1.0.dev1"
-__author__ = "John Thorvald Wodder II"
-__author_email__ = "foobar@varonathe.org"
-__license__ = "MIT"
-__url__ = "https://github.com/jwodder/pydantic-v1v2-test"
+from typing import Literal
 
 try:
     import pydantic.v1 as pydantic
 except ImportError:
-    import pydantic
+    import pydantic  # type: ignore[no-redef]
 
 
 class Foobar(pydantic.BaseModel):
+    classname: Literal["Foobar"] = pydantic.Field("Foobar", readOnly=True)
     foo: int
     bar: str
 
     @classmethod
     def load(cls, file: Path) -> Foobar:
         return cls.parse_raw(file.read_text())
+
+
+if __name__ == "__main__":
+    print(Foobar(foo=42, bar="hello"))
